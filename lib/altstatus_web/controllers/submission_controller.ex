9ab0a82e4c6649ab8d964613ee737abda70ec03e
ravi11o <ravi.suraj110@gmail.com>
2018-09-09 17:30:33 +0530

    add user authorization plugs

--- lib/altstatus_web/controllers/submission_controller.ex
+++ lib/altstatus_web/controllers/submission_controller.ex
@@ -4,6 +4,8 @@ defmodule AltstatusWeb.SubmissionController do
   alias Altstatus.Resources
   alias Altstatus.Resources.Submission
 
+  # plug :authorize_user when action in [:filter_submission]
+
   action_fallback AltstatusWeb.FallbackController
 
   def index(conn, _params) do
@@ -43,5 +45,22 @@ defmodule AltstatusWeb.SubmissionController do
   def filter_submission(conn, %{"user_id" => user_id}) do
     submissions = Resources.list_submissions(user_id)
     render(conn, "index.json", submissions: submissions)
+  end
+
+  def authorize_user(conn, _params) do
+    IO.inspect conn
+    if Enum.any?(get_req_header(conn, "token")) do
+      [token | _] = get_req_header(conn, "token")
+      case Phoenix.Token.verify(conn, "user salt", token) do
+        {:ok, _user_id} -> 
+          conn
+        {:error, _} ->
+          conn
+          |> json(%{error: "unauthorized"})
+      end
+    else 
+      conn
+      |> json(%{error: "unauthorized"})
+    end
   end
 end
