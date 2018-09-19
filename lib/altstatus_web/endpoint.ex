70bf7a9d337e570f54002c3a7df264e88372adfa
ravi11o <ravi.suraj110@gmail.com>
2018-09-19 15:46:48 +0530

    add frontend

--- lib/altstatus_web/endpoint.ex
+++ lib/altstatus_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule AltstatusWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :altstatus, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images assets js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
